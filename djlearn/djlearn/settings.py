b8d453fdca278f4e0d70581bcbb96c399301aaf1
Benjamin Slatt <benjamin@dodgersden.com>
2025-09-26 08:32:36 -0400

    Add django debug toolbar

--- djlearn/djlearn/settings.py
+++ djlearn/djlearn/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import sys
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +138,20 @@ TEMPLATES = [
         },
     },
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost',
+]
+
+TESTING = 'test' in sys.argv or 'PYTEST_VERSION' in os.environ
+
+if not TESTING:
+    INSTALLED_APPS = [
+        *INSTALLED_APPS,
+        'debug_toolbar',
+    ]
+    MIDDLEWARE = [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+        *MIDDLEWARE,
+    ]
