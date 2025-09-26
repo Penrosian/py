5383adbc90e244024d4804329fc8b8b50296300f
Benjamin Slatt <benjamin@dodgersden.com>
2025-09-26 08:51:44 -0400

    Make app a package

--- djlearn/djlearn/settings.py
+++ djlearn/djlearn/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
+    'django_polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
--- djlearn/djlearn/urls.py
+++ djlearn/djlearn/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.conf import settings
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', include('django_polls.urls')),
     path('admin/', admin.site.urls),
 ]
 
