d7e268a0d0399ff7cd9afdc853b04d49a3d46c81
Benjamin Slatt <benjamin@dodgersden.com>
2025-10-02 11:42:09 -0400

    Move polls back out of being a package

--- djlearn/djlearn/settings.py
+++ djlearn/djlearn/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_polls.apps.PollsConfig',
+    'polls.apps.PollsConfig',
     'todo.apps.TodoConfig',
 ]
 
--- djlearn/polls/apps.py
+++ djlearn/polls/apps.py
@@ -3,5 +3,4 @@ from django.apps import AppConfig
 
 class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_polls'
-    label = 'polls'
+    name = 'polls'
